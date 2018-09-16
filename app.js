bbeae53827b95447e2197500cf0b224a0c8a5131
Hobin Kang <hobinkang@v1020-wn-77-255.campus-dynamic.uwaterloo.ca>
2018-09-15 20:58:08 -0400

    changes

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/assets'));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/posenet'));
 
 // Set up database
 const mongoose = require('mongoose');
